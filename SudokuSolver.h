495b0fb39f7515b52ef92fe3c9bc6d82f7485e71
GabovYegor <gabov.egor2017@yandex.ru>
2022-08-06 12:40:17 +0300

    Implemented interface for SudokuSolver

--- SudokuSolver.h
+++ SudokuSolver.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <algorithm>
+#include <unordered_set>
+
+#include "Board.h"
+
+namespace SudokuSolver {
+    std::pair<bool, Board::Board> solve_sudoku(const Board::Board& board);
+}
--- main.cpp
+++ main.cpp
@@ -1,76 +1,4 @@
-#include <algorithm>
-#include <numeric>
-#include <unordered_set>
-
-#include "Board.h"
-
-bool verify_sudoku_row_rule(const Board::Board& board) {
-    for(const auto& row : board) {
-        const auto number_of_non_empty_el = std::count_if(row.begin(), row.end(),
-                                                          [](auto el){ return el != Board::EMPTY_CELL; });
-        std::unordered_set<char> unique_set;
-        for(const auto el : row) {
-            unique_set.emplace(el);
-        }
-        unique_set.erase(0);
-        if(unique_set.size() != number_of_non_empty_el)
-            return false;
-    }
-    return true;
-}
-
-bool verify_sudoku_column_rule(const Board::Board& board) {
-    const auto transposed_board = board.get_transposed_board();
-    return verify_sudoku_row_rule(transposed_board);
-}
-
-bool verify_sudoku_grid_rule(const Board::Board& board) {
-    // Prepare grids
-    const auto board_separated_by_grids = board.get_grids();
-    return verify_sudoku_row_rule(board_separated_by_grids);
-}
-
-bool verify_sudoku(const Board::Board& board) {
-    // 1. Verify that there are no the same elements in the row
-    if(!verify_sudoku_row_rule(board))
-        return false;
-
-    // 2. Verify that there are no the same elements in the column
-    if(!verify_sudoku_column_rule(board))
-        return false;
-
-    // 3. Verify that there are no the same elements in the grid
-    if(!verify_sudoku_grid_rule(board))
-        return false;
-
-    return true;
-}
-
-bool solve_sudoku(Board::Board& board) {
-    for(size_t i = 0; i < board.board_.size(); ++i) {
-        for (size_t j = 0; j < board.board_[i].size(); ++j) {
-            if(board.board_[i][j] == Board::EMPTY_CELL) {
-                for(char possible_sln = 1; possible_sln <= 9; ++possible_sln) {
-                    board.board_[i][j] = possible_sln;
-                    if(verify_sudoku(board)) {
-                        auto res = solve_sudoku(board);
-                        if(!res) {
-                            board.board_[i][j] = Board::EMPTY_CELL;
-                        }
-                        else {
-                            return true;
-                        }
-                    }
-                }
-                if(!verify_sudoku(board)) {
-                    board.board_[i][j] = Board::EMPTY_CELL;
-                    return false;
-                }
-            }
-        }
-    }
-    return true;
-}
+#include "SudokuSolver.h"
 
 int main() {
     Board::board_t test_board_1 = {
@@ -110,7 +38,7 @@ int main() {
     };
 
     Board::Board b { test_board_3 };
-    solve_sudoku(b);
-    b.print();
+    auto [is_resolvable, output_board] = SudokuSolver::solve_sudoku(b);
+    output_board.print();
     return 0;
 }
